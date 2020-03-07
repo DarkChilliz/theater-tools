ad5eb74148d8a8031d26ffb06e009e2a78144b5c
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-03-07 11:45:04 +1100

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
+document.getElementById("v-1").style = "width: 50%; height: 31.2%; top: 68.8%; left: 0%;";
+document.getElementById("v-2").style = "width: 50%; height: 31.2%; top: 68.8%; left: 50%;";
+
+
 
 // document.getElementsByTagName("body")[0].style.background = "#ffd700";
 // document.getElementById("v-0").style.width = "10%";
 
 // setTimeout(setStyles, 3000);
 // function setStyles() {
-//   document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
-//   document.getElementById("v-1").style = "width: 50%; height: 31.2%; top: 68.8%; left: 0%;";
-//   document.getElementById("v-2").style = "width: 50%; height: 31.2%; top: 68.8%; left: 50%;";
 // }
-document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
-document.getElementById("v-1").style = "width: 50%; height: 31.2%; top: 68.8%; left: 0%;";
-document.getElementById("v-2").style = "width: 50%; height: 31.2%; top: 68.8%; left: 50%;";
