f1480747ae247c295b280a13d0f802d41ade9a3a
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-03-11 09:06:11 +1100

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,44 @@
-// console.log("Hello World!");
-
-document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
-document.getElementById("v-1").style = "width: 50%; height: 31.2%; top: 68.8%; left: 0%;";
-document.getElementById("v-2").style = "width: 50%; height: 31.2%; top: 68.8%; left: 50%;";
-
-
-
 // document.getElementsByTagName("body")[0].style.background = "#ffd700";
-// document.getElementById("v-0").style.width = "10%";
+function getplayers() {
+    var cntr = 0;
+    for (var i = 0; i <= cntr; i++) {
+        if(document.getElementById("v-" + i) == null) {
+            break;
+        } else {
+            cntr++;
+        }
+    }
+    return cntr;
+}
 
-// setTimeout(setStyles, 3000);
-// function setStyles() {
-// }
+function setstyles(i) {
+    switch(i) {
+        case 2:
+            document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
+            document.getElementById("v-1").style = "width: 100%; height: 31.2%; top: 68.8%; left: 0%;";
+            break;
+        case 3:
+            document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
+            document.getElementById("v-1").style = "width: 50%; height: 31.2%; top: 68.8%; left: 0%;";
+            document.getElementById("v-2").style = "width: 50%; height: 31.2%; top: 68.8%; left: 50%;";
+            break;
+        case 4:
+            // temp
+            break;
+        case 5:
+            // temp
+            break;
+        case 6:
+            // temp
+            break;
+        case 7:
+            // temp
+            break;
+        case 8:
+            // temp
+            break;
+    }
+}
+
+setTimeout( setstyles(getplayers()) , 2000);
+console.log(getplayers());
