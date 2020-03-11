9e30be3fc8b3538658c683e3e953f2007424c038
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-03-11 19:27:46 +1100

    Update content.js

--- content.js
+++ content.js
@@ -1,29 +1,35 @@
 // document.getElementsByTagName("body")[0].style.background = "#ffd700";
 // https://www.silisoftware.com/tools/screen_aspect_ratio_calculator
-function FakeRound(number, decimals) {
-    var magnitude = Math.pow(10, decimals);
-    return Math.round(number * magnitude) / magnitude;
-}
+
+// function MissingValue(aspect_ratio, width, height) {
+//     if (height < 1) {
+//         height = width / aspect_ratio;
+//         console.log("MissingValue: height");
+//         return height;
+//     } else if (width < 1) {
+//         width = height * aspect_ratio;
+//         console.log("MissingValue: width");
+//         return width;
+//     } else {
+//         console.log("function MissingValue: Error");
+//     }
+// }
 
 function MissingValue(aspect_ratio, width, height) {
-    if (!width && !height) {
-        width  = 1920;
-        height = 1080;
-    }
-    if (width && height) {
-        aspect_ratio = width / height;
-        return aspect_ratio;
-    }
-    if (aspect_ratio) {
-        if (width && !height) {
-            height = width / aspect_ratio;
+	if (width && height) {
+		aspect_ratio = width / height;
+	}
+	if (aspect_ratio) {
+		if (width && !height) {
+			height = width / aspect_ratio;
             return height;
-        } else if (height && !width) {
-            width = height * aspect_ratio;
+		} else if (height && !width) {
+			width = height * aspect_ratio;
             return width;
-        }
-    }
-    return null;
+		}
+	} else {
+		console.log("function MissingValue: Error");
+	}
 }
 
 function getplayers() {
@@ -37,73 +43,69 @@ function getplayers() {
     }
     return cntr;
 }
-console.log(getplayers());
 
 function setstyles(i) {
     var clientW = document.getElementById("playdiv").clientWidth;
     var clientH = document.getElementById("playdiv").clientHeight;
-    // var w = screen.width;
-    // var h = screen.height;
     var r = 1.778;
+    var tmp = [];
     var w = [];
     var h = [];
     var t = [];
     var l = [];
-    var tmp = [];
+    function v_0_style_calc(){
+        w[0] = Math.round( clientW );
+        h[0] = Math.round( MissingValue( r, w[0], 0 ) );
+    }
+    function v_1_style_calc(){
+        h[1] = Math.round( clientH - h[0] );
+        t[1] = ( clientH - h[1] );
+    }
     switch(i) {
         case 1:
-            // w[0] = clientW;
-            w[0] = FakeRound( clientW, ((clientW < 100) ? 1 : 0));
-            h[0] = MissingValue(r, clientW, 0);
+            v_0_style_calc()
             document.getElementById("v-0").style = "width: " + w[0] + "px" + "; height: " + h[0] + "px" + "; top: 0%; left: 0%;";
             break;
         case 2:
-            w[0] = FakeRound( clientW, ((clientW < 100) ? 1 : 0));
-            h[0] = MissingValue( r, w[0], 0 );
+            v_0_style_calc()
             w[1] = clientW;
-            tmp[1] = (clientH - h[0]);
-            h[1] = FakeRound( tmp[1], ((tmp[1] < 100) ? 1 : 0));
-            t[1] = (clientH - h[1]);
+            v_1_style_calc()
             document.getElementById("v-0").style = "width: " + w[0] + "px" + "; height: " + h[0] + "px" + "; top: 0%; left: 0%;";
             document.getElementById("v-1").style = "width: " + w[1] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: 0%;";
             break;
         case 3:
-            w[0] = FakeRound( clientW, ((clientW < 100) ? 1 : 0));
-            h[0] = MissingValue( r, w[0], 0 );
-            w[1] = (clientW / 2);
-            tmp[1] = (clientH - h[0]);
-            h[1] = FakeRound( tmp[1], ((tmp[1] < 100) ? 1 : 0));
-            t[1] = (clientH - h[1]);
+            v_0_style_calc()
+            w[1] = ( clientW / 2 );
+            v_1_style_calc()
+            l[1] = ( clientW / 2 );
             document.getElementById("v-0").style = "width: " + w[0] + "px" + "; height: " + h[0] + "px" + "; top: 0%; left: 0%;";
-            document.getElementById("v-1").style = "width: " + w[1] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: 0%;";
-            document.getElementById("v-2").style = "width: " + w[1] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: 50%;";
+            document.getElementById("v-1").style = "width: " + w[1] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: " + l[1] + "px;";
+            document.getElementById("v-2").style = "width: " + w[1] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: 0%;";
             break;
         case 4:
-            document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
-            document.getElementById("v-1").style = "width: 45.4%; height: 31.2%; top: 68.8%; left: 54.6%;";
-            document.getElementById("v-2").style = "width: 27.5%; height: 31.2%; top: 68.8%; left: 27.4%;";
-            document.getElementById("v-3").style = "width: 27.4%; height: 31.2%; top: 68.8%; left: 0%;";
+            v_0_style_calc()
+            v_1_style_calc()
+            w[1] = Math.round( MissingValue( r, 0, h[1] ) );
+            l[1] = ( clientW - w[1] );
+            w[2] = ( l[1] / 2 );
+            l[2] = w[2];
+            document.getElementById("v-0").style = "width: " + w[0] + "px" + "; height: " + h[0] + "px" + "; top: 0%; left: 0%;";
+            document.getElementById("v-1").style = "width: " + w[1] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: " + l[1] + "px;";
+            document.getElementById("v-2").style = "width: " + w[2] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: " + l[2] + "px;";
+            document.getElementById("v-3").style = "width: " + w[2] + "px" + "; height: " + h[1] + "px" + "; top: " + t[1] + "px" + "; left: 0%;";
             break;
         case 5:
             // temp
             break;
         case 6:
-            document.getElementById("v-0").style = "width: 100%; height: 68.8%; top: 0%; left: 0%;";
-            document.getElementById("v-1").style = "width: 20%; height: 15.6%; top: 68.8%; left: 0%;";
-            document.getElementById("v-2").style = "width: 20%; height: 15.6%; top: 84.4%; left: 0%;";
-            document.getElementById("v-3").style = "width: 20%; height: 15.6%; top: 68.8%; left: 20%;";
-            document.getElementById("v-4").style = "width: 20%; height: 15.6%; top: 84.4%; left: 20%;";
-            document.getElementById("v-5").style = "width: 45.4%; height: 31.2%; top: 68.8%; left: 54.6%;";
-            document.getElementById("v-6").style = "width: 45.5%; height: 31.2%; top: 68.8%; left: 54.5%;";
+            // temp
             break;
         case 7:
             // temp
             break;
         case 8:
-            document.getElementById("v-5").style = "width: 20%; height: 15.6%; top: 68.8%; left: 40%;";
-            document.getElementById("v-8").style = "width: 45.5%; height: 31.2%; top: 68.8%; left: 54.5%;";
+            // temp
             break;
     }
 }
-
-setTimeout( setstyles(getplayers()) , 1000);
+setTimeout( setstyles(getplayers()) , 2000);
