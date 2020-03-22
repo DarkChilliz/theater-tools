3379791d1fd602db0d14f10cd2405f6af2be8e08
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-03-23 10:16:19 +1100

    Update main.js

--- main.js
+++ main.js
@@ -30,19 +30,27 @@ function setStylesImg() {
 
 //https://stackoverflow.com/questions/15807021
 function convertToPercentage(parentWindow, pixels) {
-    //unfinished
+    //obsolete + unfinished
     return ( parentWindow - pixels ) / parentWindow; // 0.92%
 }
 
 //https://stackoverflow.com/questions/21441777
 function getPlayers() {
+    //obsolete
     return document.querySelectorAll('[id*="v-"]');
 }
 
+function test() {
+    console.log(getPlayers());
+    console.log(chans);
+    console.log(chats);
+    console.log(fldids);
+}
+
 function setStyles() {
-    var clientW = document.getElementById("playdiv").clientWidth;
-    var clientH = document.getElementById("playdiv").clientHeight;
-    var p = getPlayers(), w = [], h = [], t = [], l = [];
+    var clientW = document.getElementById("playdiv").clientWidth
+      , clientH = document.getElementById("playdiv").clientHeight
+      , w = [], h = [], t = [], l = [];
     const r = 1.778;
     function v_0_style_calc() {
         w[0] = Math.round( clientW );
@@ -53,29 +61,29 @@ function setStyles() {
         t[1] = ( clientH - h[1] );
     }
     function writeStyle(p, w, h, t, l) {
-        document.getElementById(p).style = "width: " + w[0] + w[1] + "; height: " + h[0] + h[1] + "; top: " + t[0] + t[1] + "; left: " + l[0] + l[1] + ";";
+        document.getElementById("v-" + fldids[p]).style = "width: " + w[0] + w[1] + "; height: " + h[0] + h[1] + "; top: " + t[0] + t[1] + "; left: " + l[0] + l[1] + ";";
     }
-    switch(p.length) {
+    switch(chans.length) {
         case 0:
             break;
         case 1:
             v_0_style_calc();
-            writeStyle(p[0].id, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
             break;
         case 2:
             v_0_style_calc();
             v_1_style_calc();
-            writeStyle(p[0].id, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
-            writeStyle(p[1].id, [w[0],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[0],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
             break;
         case 3:
             v_0_style_calc();
             w[1] = ( clientW / 2 );
             v_1_style_calc();
             l[1] = ( clientW / 2 );
-            writeStyle(p[0].id, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
-            writeStyle(p[1].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
-            writeStyle(p[2].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
+            writeStyle(2, [w[1],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
             break;
         case 4:
             v_0_style_calc();
@@ -83,10 +91,10 @@ function setStyles() {
             w[1] = Math.round( missingValue(r, 0, h[1]) );
             l[1] = ( clientW - w[1] );
             l[2] = w[2] = ( l[1] / 2 );
-            writeStyle(p[0].id, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
-            writeStyle(p[1].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
-            writeStyle(p[2].id, [w[2],"px"], [h[1],"px"], [t[1],"px"], [l[2],"px"]);
-            writeStyle(p[3].id, [w[2],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
+            writeStyle(2, [w[2],"px"], [h[1],"px"], [t[1],"px"], [l[2],"px"]);
+            writeStyle(3, [w[2],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
             break;
         case 5:
             v_0_style_calc();
@@ -95,11 +103,11 @@ function setStyles() {
             l[1] = ( clientW - w[1] );
             l[2] = ( l[1] - w[1] );
             l[3] = ( l[2] - w[1] );
-            writeStyle(p[0].id, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
-            writeStyle(p[1].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
-            writeStyle(p[2].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[2],"px"]);
-            writeStyle(p[3].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[3],"px"]);
-            writeStyle(p[4].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
+            writeStyle(2, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[2],"px"]);
+            writeStyle(3, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[3],"px"]);
+            writeStyle(4, [w[1],"px"], [h[1],"px"], [t[1],"px"], ["0","%"]);
             break;
         case 6:
             v_0_style_calc();
@@ -109,12 +117,12 @@ function setStyles() {
             l[2] = w[2] = ( l[1] / 2 );
             h[2] = ( h[1] / 2 );
             t[2] = ( t[1] + h[2] );
-            writeStyle(p[0].id, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
-            writeStyle(p[1].id, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
-            writeStyle(p[2].id, [w[2],"px"], [h[2],"px"], [t[1],"px"], [l[2],"px"]);
-            writeStyle(p[3].id, [w[2],"px"], [h[2],"px"], [t[2],"px"], [l[2],"px"]);
-            writeStyle(p[4].id, [w[2],"px"], [h[2],"px"], [t[1],"px"], ["0","%"]);
-            writeStyle(p[5].id, [w[2],"px"], [h[2],"px"], [t[2],"px"], ["0","%"]);
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
+            writeStyle(2, [w[2],"px"], [h[2],"px"], [t[1],"px"], [l[2],"px"]);
+            writeStyle(3, [w[2],"px"], [h[2],"px"], [t[2],"px"], [l[2],"px"]);
+            writeStyle(4, [w[2],"px"], [h[2],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(5, [w[2],"px"], [h[2],"px"], [t[2],"px"], ["0","%"]);
             break;
         case 7:
             // temp
@@ -130,9 +138,4 @@ function setStyles() {
 
 setStylesImg();
 setStyles();
-
-
-console.log(getPlayers());
-console.log(chans);
-console.log(chats);
-console.log(fldids);
+test();
