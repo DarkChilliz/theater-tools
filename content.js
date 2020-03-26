e7162d51cfbc6eff9e0fba1c33ee6e5540beb0bd
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-03-27 00:48:43 +1100

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
 //https://stackoverflow.com/questions/9515704
-function injectMainScript () {
+(function () {
     var s = document.createElement('script');
     s.src = chrome.runtime.getURL('main.js');
     s.onload = function() {
         this.remove();
     };
     (document.head || document.documentElement).appendChild(s);
-}
-
-injectMainScript();
+})();
