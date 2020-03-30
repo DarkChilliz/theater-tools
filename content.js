852561d59149d486976e011093b45a500837090e
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-03-30 14:26:34 +1100

    Update content.js

--- content.js
+++ content.js
@@ -3,7 +3,15 @@
     var s = document.createElement('script');
     s.src = chrome.runtime.getURL('main.js');
     s.onload = function() {
+        // var url=chrome.runtime.getURL("menu.png");
+
+        // var evt=document.createEvent("CustomEvent");
+        // evt.initCustomEvent("yourCustomEvent", true, true, url);
+        // document.dispatchEvent(evt);
+
         this.remove();
     };
     (document.head || document.documentElement).appendChild(s);
-})(); // https://github.com/DarkChilliz/chrome-extension-twitchtheater.tv
+})();
+
+// "https://github.com/DarkChilliz/chrome-extension-twitchtheater.tv"
