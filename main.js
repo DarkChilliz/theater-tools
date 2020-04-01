81ca81b2f84ee326f65d4a743eb8561478003b73
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-04-02 08:36:49 +1100

    Update main.js

--- main.js
+++ main.js
@@ -30,6 +30,22 @@ function setStyles() {
         h[1] = Math.round( clientH - h[0] );
         t[1] = ( clientH - h[1] );
     }
+    function v_4_5_6_style_calc() {
+        w[1] = Math.round( missingValue(r, 0, h[1]) );
+        l[1] = ( clientW - w[1] );
+    }
+    function v_4_6_style_calc() {
+        l[2] = w[2] = ( l[1] / 2 );
+    }
+    function v_7_8_style_calc() {
+        w[3] = Math.round( missingValue(r, 0, (h[1] / 2)) );
+        l[2] = ( w[3] * 2 );
+        l[3] = w[3];
+    }
+    function v_7_8_9_style_calc() {
+        h[3] = ( h[1] / 2 );
+        t[2] = ( t[1] + h[3] );
+    }
     function writeStyle(p, w, h, t, l) {
         var obj = document.getElementById("v-" + fldids[p]);
         obj.style = "width: " + w[0] + w[1] +
@@ -62,9 +78,8 @@ function setStyles() {
         case 4:
             v_0_style_calc();
             v_1_style_calc();
-            w[1] = Math.round( missingValue(r, 0, h[1]) );
-            l[1] = ( clientW - w[1] );
-            l[2] = w[2] = ( l[1] / 2 );
+            v_4_5_6_style_calc();
+            v_4_6_style_calc();
             writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
             writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
             writeStyle(2, [w[2],"px"], [h[1],"px"], [t[1],"px"], [l[2],"px"]);
@@ -73,8 +88,7 @@ function setStyles() {
         case 5:
             v_0_style_calc();
             v_1_style_calc();
-            w[1] = Math.round( missingValue(r, 0, h[1]) );
-            l[1] = ( clientW - w[1] );
+            v_4_5_6_style_calc();
             h[3] = ( h[1] / 2 );
             w[3] = Math.round( missingValue(r, 0, (h[1] / 2)) );
             l[2] = ( l[1] - w[3] );
@@ -90,8 +104,7 @@ function setStyles() {
         case 6:
             v_0_style_calc();
             v_1_style_calc();
-            w[1] = Math.round( missingValue(r, 0, h[1]) );
-            l[1] = ( clientW - w[1] );
+            v_4_5_6_style_calc();
             l[2] = w[2] = ( l[1] / 2 );
             h[2] = ( h[1] / 2 );
             t[2] = ( t[1] + h[2] );
@@ -105,13 +118,10 @@ function setStyles() {
         case 7:
             v_0_style_calc();
             v_1_style_calc();
-            h[3] = ( h[1] / 2 );
-            w[3] = Math.round( missingValue(r, 0, (h[1] / 2)) );
+            v_7_8_style_calc();
+            v_7_8_9_style_calc();
             w[2] = w[1] = ( (clientW - (w[3] * 2)) / 2 );
             l[1] = ( clientW - w[1] );
-            l[2] = ( l[1] - w[1] );
-            t[2] = ( t[1] + h[3] );
-            l[3] = w[3];
             writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
             writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
             writeStyle(2, [w[2],"px"], [h[1],"px"], [t[1],"px"], [l[2],"px"]);
@@ -121,13 +131,41 @@ function setStyles() {
             writeStyle(6, [w[3],"px"], [h[3],"px"], [t[2],"px"], ["0","%"]);
             break;
         case 8:
-            // temp
+            v_0_style_calc();
+            v_1_style_calc();
+            v_7_8_style_calc();
+            v_7_8_9_style_calc();
+            w[1] = ( clientW - (w[3] * 3) );
+            l[1] = ( w[3] * 3 );
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[1],"px"], [h[1],"px"], [t[1],"px"], [l[1],"px"]);
+            writeStyle(2, [w[3],"px"], [h[3],"px"], [t[1],"px"], [l[2],"px"]);
+            writeStyle(3, [w[3],"px"], [h[3],"px"], [t[2],"px"], [l[2],"px"]);
+            writeStyle(4, [w[3],"px"], [h[3],"px"], [t[1],"px"], [l[3],"px"]);
+            writeStyle(5, [w[3],"px"], [h[3],"px"], [t[2],"px"], [l[3],"px"]);
+            writeStyle(6, [w[3],"px"], [h[3],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(7, [w[3],"px"], [h[3],"px"], [t[2],"px"], ["0","%"]);
             break;
         case 9:
-            //temp
+            v_0_style_calc();
+            v_1_style_calc();
+            w[3] = ( clientW / 4 );
+            v_7_8_9_style_calc();
+            l[1] = ( w[3] * 3 );
+            l[2] = ( w[3] * 2 );
+            l[3] = w[3];
+            writeStyle(0, [w[0],"px"], [h[0],"px"], ["0","%"], ["0","%"]);
+            writeStyle(1, [w[3],"px"], [h[3],"px"], [t[1],"px"], [l[1],"px"]);
+            writeStyle(2, [w[3],"px"], [h[3],"px"], [t[2],"px"], [l[1],"px"]);
+            writeStyle(3, [w[3],"px"], [h[3],"px"], [t[1],"px"], [l[2],"px"]);
+            writeStyle(4, [w[3],"px"], [h[3],"px"], [t[2],"px"], [l[2],"px"]);
+            writeStyle(5, [w[3],"px"], [h[3],"px"], [t[1],"px"], [l[3],"px"]);
+            writeStyle(6, [w[3],"px"], [h[3],"px"], [t[2],"px"], [l[3],"px"]);
+            writeStyle(7, [w[3],"px"], [h[3],"px"], [t[1],"px"], ["0","%"]);
+            writeStyle(8, [w[3],"px"], [h[3],"px"], [t[2],"px"], ["0","%"]);
             break;
         default:
-            console.log("setStyles(): error")
+            console.log("setStyles(): player number out of range")
     }
 }
 
