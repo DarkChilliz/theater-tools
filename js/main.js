af232b08fb6630dd98595988bbfab861ebd90708
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2020-12-31 07:50:37 +1100

    .

--- js/main.js
+++ js/main.js
@@ -164,29 +164,25 @@ function chgPlayerStyle() {
                 t[2] = t[1] + h[2];
                 //
                 w[1] = Math.round( missingValue( r, 0, h[1] ) );
-                w[2] = Math.round( missingValue( r, 0, h[2] ) );
-                w[3] = clientW - ( w[1] + w[2] );
-                w[4] = w[3] / 2;
                 //
-                h[4] = Math.round( missingValue( r, w[4], 0 ) );
-                h[3] = h[1] - h[4];
-                t[3] = t[1] + h[3];
-                //
-                l[2] = w[3];
-                l[1] = w[4];
-                l[0] = w[3] + w[2];
+                l[0] = clientW - w[1];
+                l[1] = l[0] - w[1];
+                l[2] = l[1] - w[1];
+                l[3] = l[2] - w[1];
+                w[2] = l[3];
                 //top
                 writeStyle(0, w[0], h[0], 0, 0);
                 //stack 1
                 writeStyle(1, w[1], h[1], t[1], l[0]);
                 //stack 2
-                writeStyle(3, w[2], h[2], t[1], l[2]);
-                writeStyle(4, w[2], h[2], t[2], l[2]);
-                //stack 3 top
-                writeStyle(2, w[3], h[3], t[1], 0);
-                //stack 3 bot
-                writeStyle(5, w[4], h[4], t[3], l[1]);
-                writeStyle(6, w[4], h[4], t[3], 0);
+                writeStyle(2, w[1], h[1], t[1], l[1]);
+                //stack 3
+                writeStyle(3, w[1], h[1], t[1], l[2]);
+                //stack 4
+                writeStyle(4, w[1], h[1], t[1], l[3]);
+                //stack 5
+                writeStyle(5, w[2], h[2], t[1], 0);
+                writeStyle(6, w[2], h[2], t[2], 0);
                 break;
             case 8:
                 w[0] = clientW;
