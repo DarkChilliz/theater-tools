019e7cb15c1923d5291a2f3496079617f6f612b4
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-09-30 03:32:51 +1000

    Create custom-styles-ext-twitchtheater.tv.user.js

---
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        New script
+// @namespace   Violentmonkey Scripts
+// @match       *://*/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 30/09/2022, 3:29:15 am
+// ==/UserScript==
