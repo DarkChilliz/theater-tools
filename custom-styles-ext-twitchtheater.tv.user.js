fed92185e7156c267412325a6d6042129f495c8b
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-09-30 03:36:02 +1000

    Update custom-styles-ext-twitchtheater.tv.user.js

--- custom-styles-ext-twitchtheater.tv.user.js
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        custom-styles-ext-twitchtheater.tv.user.js
-// @namespace   https://github.com/DarkChilliz
-// @match        *://*twitchtheater.tv/*
-// @grant       none
-// @version     1.0
-// @author      -
-// @description 30/09/2022, 3:29:15 am
+// @name         custom-styles-ext-twitchtheater.tv.user.js
+// @namespace    https://github.com/DarkChilliz
+// @match         *://*twitchtheater.tv/*
+// @grant        none
+// @version      1.0
+// @author       -
+// @description  30/09/2022, 3:29:15 am
 // ==/UserScript==
