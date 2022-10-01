325e23669c5ea3dee85029a15d35bd75bc67a48d
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-10-01 14:22:20 +1000

    Update custom-styles-ext-twitchtheater.tv.user.js

--- custom-styles-ext-twitchtheater.tv.user.js
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -4,7 +4,7 @@
 // @match        *://*.twitchtheater.tv/*
 // @run-at       document-idle
 // @grant        none
-// @version      0.0.18
+// @version      0.0.19
 // @downloadURL  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/custom-styles-ext-twitchtheater.tv.user.js
 // @author       darkchilliz
 // @description  30/09/2022, 3:29:15 am | custom-styles-ext-twitchtheater.tv.user.js
