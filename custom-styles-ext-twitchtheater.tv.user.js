7c9b6e07534e1d386f3ae938716abd8b594339d4
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-10-02 18:52:21 +1100

    custom-styles-ext-twitchtheater.tv.user.js

--- custom-styles-ext-twitchtheater.tv.user.js
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -31,18 +31,78 @@ function writeStyleElement(content) {
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
 
-}
-
-
-
-
-
-}
-
-
-    var head = document.head || document.getElementsByTagName('head')[0],
-        style = document.createElement('style');
-
     head.appendChild(style);
     style.appendChild(document.createTextNode(css));
 }
+
+function onReceiveImgURL(playerStyleImg) { //functionsMenuImg
+    // document.getElementById("funcMenuDiv").outerHTML = funcMenuDivHtml;
+
+    var playerStyleImgObj = document.getElementById("playerStyleImg");
+        // functionsMenuImgObj = document.getElementById("functionsMenuImg");
+
+    //playerStyleImg
+    playerStyleImgObj.src = playerStyleImg;
+
+    //functionsMenuImg
+    // functionsMenuImgObj.src = functionsMenuImg;
+
+    var event = new CustomEvent("triggerScript");
+    document.dispatchEvent(event);
+}
+
+(function () {
+    'use strict';
+
+    //https://www.tampermonkey.net/documentation.php#GM_getResourceText
+    //https://www.tampermonkey.net/documentation.php#GM_getResourceURL
+    //https://www.tampermonkey.net/documentation.php#_resource
+    //http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/
+    createFuncMenuDiv(GM_getResourceText("html"));
+    writeStyleElement(GM_getResourceText("css"));
+
+    var script = document.createElement('script'),//https://stackoverflow.com/questions/9515704
+
+        //content.html
+        txtFile = new XMLHttpRequest(),
+        sContentHTML = GM_getResourceURL("html"), //"html/content.html",
+
+        //createFuncMenuDiv()
+        sPlayerStyleURL = GM_getResourceURL("icon"), //"img/playerstyle.png",
+        // sFunctionsMenuURL = GM_getResourceURL("name"); //"img/functionsmenu.png",
+
+        //main.css
+        sMainCSS_URL = GM_getResourceURL("css"), //"css/main.css",
+
+        //main.js
+        sScriptURL = GM_getResourceURL("main"); //"js/main.js";
+        script.src = GM_getResourceURL("main"); //chrome.runtime.getURL(sScriptURL);
+
+    //content.html
+    // txtFile.open("GET", sContentHTML, true);//https://forums.tumult.com/t/2129
+    // txtFile.onreadystatechange = function () {
+    //     if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse
+    //         if (txtFile.status === 200) {  // Makes sure its found the file
+    //             script.onload = function() {
+    //                 onReceiveImgURL(txtFile.responseText, sPlayerStyleURL); //sFunctionsMenuURL
+    //                 this.remove();
+    //             };
+    //         }
+    //     }
+    // }
+    // txtFile.send(null);
+
+    //main.js
+    script.onload = function() {
+        onReceiveImgURL(sPlayerStyleURL); //sFunctionsMenuURL
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(script);
+
+    //main.css
+    // var style = document.createElement('link');//https://stackoverflow.com/questions/9721344
+    // style.rel = 'stylesheet';
+    // // style.type = 'text/css';
+    // style.href = sMainCSS_URL;
+    // (document.head || document.documentElement).appendChild(style);
+})();
