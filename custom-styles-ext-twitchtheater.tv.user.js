a60c0333f303a1c28e4040919080b53474153319
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-10-02 16:45:30 +1100

    Update custom-styles-ext-twitchtheater.tv.user.js

--- custom-styles-ext-twitchtheater.tv.user.js
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -4,17 +4,17 @@
 // @match          *://*.twitchtheater.tv/*
 // @run-at         document-idle
 // @grant          none
-// @version        0.2.0.0010
+// @version        0.2.1.0002
 // @updateURL
 // @downloadURL    http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/custom-styles-ext-twitchtheater.tv.user.js
 // @author         darkchilliz
 // @description    30/09/2022, 3:29:15 am | custom-styles-ext-twitchtheater.tv.user.js
+// @grant          GM_getResourceText
+// @grant          GM_getResourceURL
 // @resource icon  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/img/playerstyle.png
 // @resource html  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/html/content.html
 // @resource css   http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/css/main.css
 // @resource main  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/js/main.js
-// @grant          GM_getResourceText
-// @grant          GM_getResourceURL
 // ==/UserScript==
 
 function createFuncMenuDiv() {
