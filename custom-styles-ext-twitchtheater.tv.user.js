3dd79ec09fdbef1063db7ed7bddac6b8b518af6b
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-10-02 15:42:56 +1100

    Update custom-styles-ext-twitchtheater.tv.user.js

--- custom-styles-ext-twitchtheater.tv.user.js
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name         twitchtheatertv-tools.user.js
-// @namespace    http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv | https://github.com/DarkChilliz
-// @match        *://*.twitchtheater.tv/*
-// @run-at       document-idle
-// @grant        none
-// @version      0.2.0
+// @name           twitchtheatertv-tools.user.js
+// @namespace      http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv | https://github.com/DarkChilliz
+// @match          *://*.twitchtheater.tv/*
+// @run-at         document-idle
+// @grant          none
+// @version        0.2.0.0010
 // @updateURL
-// @downloadURL  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/custom-styles-ext-twitchtheater.tv.user.js
-// @author       darkchilliz
-// @description  30/09/2022, 3:29:15 am | custom-styles-ext-twitchtheater.tv.user.js
-// @resource icon http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/img/playerstyle.png
-// @resource html http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/html/content.html
-// @resource css http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/css/main.css
-// @resource main http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/js/main.js
+// @downloadURL    http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/custom-styles-ext-twitchtheater.tv.user.js
+// @author         darkchilliz
+// @description    30/09/2022, 3:29:15 am | custom-styles-ext-twitchtheater.tv.user.js
+// @resource icon  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/img/playerstyle.png
+// @resource html  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/html/content.html
+// @resource css   http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/css/main.css
+// @resource main  http://192.168.1.200:8080/custom-styles-ext-twitchtheater.tv/js/main.js
+// @grant          GM_getResourceText
+// @grant          GM_getResourceURL
 // ==/UserScript==
 
 function createFuncMenuDiv() {
