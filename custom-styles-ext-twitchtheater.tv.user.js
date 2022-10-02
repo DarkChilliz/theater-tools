dcaeaecc2e59c5040b0bcfe25a8282ba68f7b0c6
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-10-02 17:11:20 +1100

    Update custom-styles-ext-twitchtheater.tv.user.js

--- custom-styles-ext-twitchtheater.tv.user.js
+++ custom-styles-ext-twitchtheater.tv.user.js
@@ -25,106 +25,15 @@ function createFuncMenuDiv(content) {
     document.getElementById("funcMenuDiv").outerHTML = content;
 }
 
-
-function writeStyleElement() {
+function writeStyleElement(content) {
     //https://stackoverflow.com/questions/524696
-    var css = `
-.bar1, .bar2, .bar3 {
-    width: 35px;
-    height: 5px;
-    background-color: #333;
-    margin: 6px 0;
-    transition: 0.4s;
-}
 
-.change .bar1 {
-    transform: translate(0, 11px) rotate(-45deg);
-}
-
-.change .bar2 {opacity: 0;}
-
-.change .bar3 {
-    transform: translate(0, -11px) rotate(45deg);
 }
 
 
-.functionsMenuImg, #playerStyleImg {
-    position: absolute;
-    /* visibility: hidden; */
-    opacity: 0.5;
-}
 
-.functionsMenuImg {
-    display: inline-block;
-    cursor: pointer;
-    left: 50px;
-    top: 5px;
-}
 
-#funcMenuDiv {
-    position: absolute;
-    overflow: hidden;
-    min-width: 86px;
-    min-height: 50px;
-    right: 254px;
-    top: 50px;
-}
 
-#funcMenuTop {
-    text-align: center;
-    font-size: 16px;
-    font-weight: bold;
-    padding-top: 15px;
-    height: 35px;
-    /* position: relative; */
-    /* top: 50px; */
-}
-
-#funcMenuBot {
-    font-size: 0.8em;
-    line-height: 1.5em;
-    overflow: auto;
-    min-height: 70px;
-    /* position: relative; */
-    /* top: 100px; */
-}
-
-#fm0 {
-    /* padding: 10px 10px; */
-    padding: 6px 6px;
-}
-
-.funcBtn:hover {background: rgb(73, 73, 73);}
-.funcBtn:focus {background: rgb(50, 50, 50);}
-.funcBtn:active {color: lightcoral;}
-.funcBtn {
-    border: none;
-    background-color: rgb(56, 56, 56); /* background-color: inherit; */
-    color: whitesmoke;
-    /* padding: 14px 28px; */
-    padding: 12px 12px;
-    font-size: 16px;
-    cursor: pointer;
-    /* display: inline-block; */
-    /* display: block; */
-}
-.active {color: lightcoral;}
-
-/* .success {color: green;}
-.info {color: dodgerblue;}
-.warning {color: orange;}
-.danger {color: red;}
-.default {color: black;} */
-
-.btnInner, #btnLine1, #btnLine2, #btnLine3, #btnLine4 {
-    display: inline-block;
-}
-#btnLine1, #btnLine2, #btnLine3, #btnLine4 {
-    /* text-align: center; */
-    width: 100%;
-}
-#btnLine2, #btnLine3, #btnLine4 {
-    padding-top: 4px;
 }
 
 `;
