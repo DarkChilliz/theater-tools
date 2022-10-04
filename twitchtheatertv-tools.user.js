b36711e227ba42fb229c34e275ddb41d3887dc8f
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2022-10-04 17:42:35 +1100

    Update twitchtheatertv-tools.user.js

--- twitchtheatertv-tools.user.js
+++ twitchtheatertv-tools.user.js
@@ -8,7 +8,7 @@
 // @version         1.1.0
 // @updateURL
 // @downloadURL     https://github.com/DarkChilliz/twitchtheatertv-tools/raw/main/twitchtheatertv-tools.user.js
-// @author          darkchilliz
+// @author          https://github.com/DarkChilliz
 // @description     30/09/2022, 3:29:15 am | custom-styles-ext-twitchtheater.tv.user.js
 // @grant           GM_getResourceText
 // @grant           GM_getResourceURL
