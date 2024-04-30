8d24841569b882f059853360631bac383f5d8f71
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2024-04-30 23:56:27 +1000

    Update twitchtv-autoreload.user.js

--- twitchtv-autoreload.user.js
+++ twitchtv-autoreload.user.js
@@ -5,21 +5,42 @@
 // @match           *://*.twitch.tv/*
 // @run-at          document-idle
 // @grant           none
-// @version         0.1.0
+// @version         1.0.1
 // @updateURL
 // @downloadURL     https://github.com/DarkChilliz/twitchtheatertv-tools/raw/main/twitchtv-autoreload.user.js
 // @author          https://greasyfork.org/en/scripts/472868-twitch-auto-reload-when-k-error/code
 // @description     Auto reload when *k error
-// @icon            https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
+// @icon
 // ==/UserScript==
+
+function styledConsoleLog(module, func, log) {
+    console.log("%c" + module + " [%c" + func + "%c]: %c" + log, "color:#755000; font-weight:bold", "color:#999999", "color:#755000; font-weight:bold", "color:#999999");
+}
 
 (function() {
     'use strict';
-    setInterval(() => {
-        var button = document.querySelector(".player-overlay-background button");
-        if (button) {
-            button.click();
-        }
-    }, 3500); //Default: 1000
-    // Your code here...
+
+    const id = document.querySelector("#live-page-chat");
+    const loc = window.location; //https://developer.mozilla.org/en-US/docs/Web/API/Location#examples
+    const params = new URLSearchParams(loc.search); //https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/get#examples
+    const channelNameEmbed = params.get("channel"); //https://dev.twitch.tv/docs/embed/video-and-clips/#interactive-frames-for-live-streams-and-vods
+    const channelNameTwitch = (loc.pathname).replace("/","");
+    const isEmbed = (loc.hostname == "player.twitch.tv");
+    const channelName = (channelNameEmbed || channelNameTwitch);
+
+    styledConsoleLog("TTVAutoReload", loc.href, "Checking for [https://player.twitch.tv/?channel=]: " + isEmbed + " (" + channelNameEmbed + ")");
+    styledConsoleLog("TTVAutoReload", loc.href, "Checking for [#live-page-chat]: " + (id ? "true" : "false"));
+
+    if (id || isEmbed && channelNameEmbed) {
+        styledConsoleLog("TTVAutoReload", loc.href, "Initialising... " + channelName);
+        setInterval(() => {
+            var button = document.querySelector(".player-overlay-background button:not(.qeepv)"); //https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+            if (button) {
+                button.click();
+                styledConsoleLog("TTVAutoReload", loc.href, "Reloading... " + channelName);
+            }
+        }, 3500); //default: 1000
+    } else {
+        styledConsoleLog("TTVAutoReload", loc.href, "Skipping...");
+    }
 })();
