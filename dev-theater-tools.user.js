c5a58a72327d9c9ff20ed7e8b3a826d63f7188e9
DarkChilliz <33816146+DarkChilliz@users.noreply.github.com>
2025-05-16 05:26:20 +1000

    2.10.12
    add case 29 to chgPlayerStyle
    
    clean up code

--- dev-theater-tools.user.js
+++ dev-theater-tools.user.js
@@ -6,7 +6,7 @@
 // @match           *://*.darkchilliz.github.io/*
 // @run-at          document-idle
 // @grant           none
-// @version         2.10.11.0000
+// @version         2.10.12.0000
 // @updateURL
 // @downloadURL     http://localhost:8020/theater-tools/dev-theater-tools.user.js
 // @author          http://localhost:8020/
